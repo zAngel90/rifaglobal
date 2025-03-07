9694bd9b2e7ec0350dfeac0b3f5c51afd120a725
zAngel90 <zangel90@gmail.com>
2025-03-07 00:01:19 -0500

    Configuración inicial para despliegue en rifas

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+import https from 'https'
 
 export const API_URL = 'https://93.127.203.121';
 
 // Configurar Axios para aceptar certificados autofirmados
-axios.defaults.httpsAgent = new (require('https').Agent)({  
+axios.defaults.httpsAgent = new https.Agent({  
   rejectUnauthorized: false
 });
 
