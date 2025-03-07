ce2914309122e12ac6b5b1e77308d32a1e3f9377
zAngel90 <zangel90@gmail.com>
2025-03-06 23:59:50 -0500

    Configuración inicial para despliegue en Verce2232323232323222l

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
+import axios from 'axios'
+
 export const API_URL = 'https://93.127.203.121';
+
+// Configurar Axios para aceptar certificados autofirmados
+axios.defaults.httpsAgent = new (require('https').Agent)({  
+  rejectUnauthorized: false
+});
 
 // URLs para imágenes
 export const getImageUrl = (path) => {
